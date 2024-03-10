63f0b483949f147b642002cfc04bc544eddfc744
P karthik <153220834+karthik-x8@users.noreply.github.com>
2024-03-10 06:46:47 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,16 @@
+function appendToDisplay(input) {
+   const display = document.getElementById("display");
+    display.value += input;   
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
